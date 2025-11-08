ea762b95e1cc2e79065f2a1f064c91c4ea9a1fc7
Vishal Shaw <vishal@Vishals-MacBook-Air.local>
2025-11-08 13:25:25 +0530

    changes

---
+++ Terraform/modules/ec2-instance/outputs.tf
@@ -0,0 +1,9 @@
+output "instance_id" {
+  description = "The ID of the EC2 instance."
+  value       = aws_instance.this.id
+}
+
+output "public_ip" {
+  description = "The public IP address of the EC2 instance."
+  value       = aws_instance.this.public_ip
+}
---
+++ Terraform/modules/ec2-instance/variables.tf
@@ -0,0 +1,14 @@
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance. Use a free tier eligible AMI."
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type. Default is t2.micro (free tier eligible)."
+  type        = string
+}
+
+variable "instance_name" {
+  description = "Name tag for the EC2 instance."
+  type        = string
+}
